932581cce7c59b347c374850d306127915097488
yapitaku9 <yapitaku9@gmail.com>
2026-01-20 14:29:09 +0900

    基礎要件、終了！

---
+++ app.py
@@ -0,0 +1,30 @@
+from db_config import User
+
+introduction = f"""===== Welcome to CRM Application =====
+[S]how: Show all users info
+[A]dd: Add new user
+[Q]uit: Quit The Application
+======================================"""
+
+print(introduction)
+
+while True:
+
+    your_command = input("your command > ")
+
+    if your_command == "S":
+        users = User.select()
+        for person in users:
+            print(f"Name: {person.user} Age: {person.age}")
+
+    elif your_command == "A":
+        new_user_name = input("New User Name > ")
+        new_user_age = input("New User Age > ")
+        User.create(user=new_user_name, age=new_user_age)
+        print(f"Add New User: {new_user_name}")
+
+    elif your_command == "Q":
+        break
+
+    else:
+        break
---
+++ db_config.py
@@ -0,0 +1,29 @@
+import os
+
+# import sys
+from dotenv import load_dotenv
+from peewee import CharField, IntegerField, Model
+from playhouse.db_url import connect
+
+# .envの読み込み
+load_dotenv(override=True)
+
+# データベースへの接続
+# db = SqliteDatabase("peewee_db.sqlite")
+db = connect(os.environ.get("DATABASE"))
+# db = connect(os.environ.get("DATABASE") or "sqlite://peewee_db.sqlite")
+
+
+# メッセージのモデル
+class User(Model):
+
+    id = IntegerField(primary_key=True)
+    user = CharField()
+    age = CharField()
+
+    class Meta:
+        database = db
+        table_name = "user"
+
+
+db.create_tables([User])
